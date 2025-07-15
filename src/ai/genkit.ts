d1a99d74ca10c18570234253c389ced732ac8533
Mr-Prince16 <srinjoybhattacharya9@gmail.com>
2025-07-15 07:27:46 +0530

    genkit change model overloaded

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -17,5 +17,5 @@ export const ai = genkit({
   // By default, Genkit will use the gemini-1.5-flash model.
   // You can specify a different model here, or on a per-request basis.
   // The model can be configured via the GENKIT_MODEL environment variable.
-  model: process.env.GENKIT_MODEL || 'googleai/gemini-1.5-flash',
+  model: process.env.GENKIT_MODEL || 'googleai/gemini-2.5-flash',
 });
